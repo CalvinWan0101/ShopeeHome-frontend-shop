15ee590d0c4e1a7c7f7d189bf96cd88e436bb534
yclin <t110820059@ntut.org.tw>
2024-01-09 16:08:01 +0800

    [Refactoring] Removed unused files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,21 @@
 import './App.scss'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import LogIn from './LogIn'
-import Home from './Home'
-import Product from './Product'
-import Shop from './ShopPage/Shop.tsx'
-import Test from './test.tsx' //TODO: delete me
 import Seller from './Seller'
-import { ThemeProvider, createTheme } from "@mui/material";
-import CssBaseline from '@mui/material/CssBaseline';
-import { MaterialTailwindTheme, ThemeProvider as ThemeProviderTailwind} from "@material-tailwind/react";
 import Header from './Header'
-import UserInformation from './UserInformation.tsx'
-import Shoppingcart from './ShoppingcartPage/Shoppingcart.tsx'
-
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-})
-
-const MyTheme = createTheme({
-  palette: {
-    background: {
-      default: '#fef9eb',
-    },
-  },
-});
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='' element={<Header/>}>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/Product/:id' element={<Product/>}></Route>
-            <Route path='/login' element={<LogIn/>}></Route>
-            <Route path='/PersionalInformation' element={<UserInformation/>}></Route>
-            <Route path='/ShoppingCart' element={<Shoppingcart/>}></Route>
-            <Route path='/Shop/:id' element={<Shop/>}></Route>
-            <Route path='/test' element={<Test/>}></Route>
-            <Route path='/Seller/:id' element={<Seller/>}></Route>
+          <Route path='' element={<Header />}>
+            <Route path='/login' element={<LogIn />}></Route>
+            <Route path='/Seller/:id' element={<Seller />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
